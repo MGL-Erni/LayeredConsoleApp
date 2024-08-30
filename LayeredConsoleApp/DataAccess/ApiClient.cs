2ffa6fb778d9152b48bfd4497d37d94ba7222f66
MGL-Erni <matthew.lucos@betterask.erni>
2024-08-30 19:00:08 +0800

    first

---
+++ LayeredConsoleApp/DataAccess/ApiClient.cs
@@ -0,0 +1,27 @@
+﻿namespace LayeredConsoleApp.DataAccess
+{
+    internal static class ApiEndpoints
+    {
+        public const string Users = "https://fake-json-api.mock.beeceptor.com/users";
+    }
+
+    internal class ApiClient
+    {
+        // readonly: can only be assigned via declaration or within the constructor
+        private readonly HttpClient _httpClient;
+
+        public ApiClient()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        // Task<what it returns when awaited>
+        // HTTP requests should be async, waits for external process
+        public async Task<string> GetUsersAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(ApiEndpoints.Users);
+            response.EnsureSuccessStatusCode();
+            return awiat;
+        }
+    }
+}
